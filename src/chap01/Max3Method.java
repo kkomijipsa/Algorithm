6807495028efbd529c5c6726313810c5c30a505d
RSH <fbtnghks7906@gmail.com>
2024-02-08 23:25:11 +0900

    파일 이름 변경

--- src/chap01/Max3Method.java
+++ src/chap01/Max3Method.java
@@ -1,7 +1,7 @@
 package chap01;
 // 3개의 정숫값 가운데 최댓값을 구하여 출력합니다.
 
-public class Max3m {
+public class Max3Method {
   // a,b,c의 최댓값을 구하여 반환합니다.
   static int max3(int a, int b, int c) {
     int max = a;
